feba645885f0a1b8d0785b898aec87fb97b52dd2
papalouis <papa.louis59@gmail.com>
2018-10-14 23:26:52 +0200

    function next() + fix minor issues

--- podcast.js
+++ podcast.js
@@ -1,3 +1,6 @@
+//liens testés : http://rss.cnn.com/services/podcasting/studentnews/rss.xml
+//http://radiofrance-podcast.net/podcast09/rss_17974.xml
+
 function play(idPlayer, control) {
     var player = document.querySelector('#' + idPlayer);
     var getBtn = document.getElementById('playpause');
@@ -17,38 +20,102 @@ function play(idPlayer, control) {
 function stoped(idPlayer) {
     var player = document.querySelector('#' + idPlayer);
     var progress = document.querySelector('#progress');
-
+    var getBtn = document.getElementById('playpause');
+    var getIcon = document.getElementById('icon');
+    if (player.play){
+      getIcon.classList.remove('fa-pause');
+      getIcon.classList.add('fa-play');
+    }
     player.currentTime = 0;
     player.pause();
     progress.value = 0;
+
 }
 
 var videoCount = 0;
+//pas très propre à notre avis car demande de refaire une requete
+function next(idPlayer) {
+  var player = document.querySelector('#' + idPlayer);
+  var getBtn = document.getElementById('playpause');
+  var getIcon = document.getElementById('icon');
+  //Clear title
+  document.getElementById('title').innerHTML = "";
+  //icon
+  if (player.play){
+    getIcon.classList.remove('fa-play');
+    getIcon.classList.add('fa-pause');
+  }
+  player.pause();
+  //clear video
+  player.innerHTML = " ";
+  //start
+  //array de lien
+  var links = [];
+  //array de titre
+  var titles = [];
+  var requete_ajax = new XMLHttpRequest();
+  var clean_url = document.querySelector('#rsslink').value;
+  var url = "https://cors-anywhere.herokuapp.com/"+clean_url;
+  requete_ajax.open('GET', url, true);
+  requete_ajax.send();
+  requete_ajax.onload = function(){
+  if (requete_ajax.status === 200){
+    fetch(url).then((res) => {
+      res.text().then((xmlTxt) => {
+      var domParser = new DOMParser()
+      let doc = domParser.parseFromString(xmlTxt, 'text/xml')
+      doc.querySelectorAll('item').forEach((item) => {
+        //récupération des liens
+        var enclosure = item.getElementsByTagName("enclosure")[0];
+        var link = enclosure.attributes[0].value;
+        //récupération des titres
+        var current_title = item.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+        //stockage dans les arrays
+        titles.unshift(current_title);
+        links.unshift(link);
+      });
+      //incrémentation
+      videoCount ++;
+      link = links[videoCount];
+      title = titles[videoCount];
+      //affichage titre et vidéo suivante
+      document.getElementById('title').innerHTML = title;
+      player.innerHTML = "<source src=" + link + "type=video/mp4>";
+      player.load();
+      });
+    });
+  }
+};
+}
 
-/*function next(idPlayer) {
-  videoCount ++;
-  console.log(list);
-  var item_current = items[videoCount];
-  console.log(item_current);
-  var enclosure = item.getElementsByTagName("enclosure")[0];
-  var link = enclosure.attributes[0].value;
-  document.getElementById("videoPlayer").innerHTML = "";
-  document.getElementById("videoPlayer").innerHTML = "<source src=" + link + "type=video/mp4>";
-}*/
-
+//bouton mute / unmute
 function muted(idPlayer) {
     var player = document.querySelector('#' + idPlayer);
     player.muted = !player.muted ;
 }
 
-function addRss(){
+//ajout d'un lien RSS
+function addRss(idPlayer){
+  var player = document.querySelector('#' + idPlayer);
+  var getBtn = document.getElementById('playpause');
+  var getIcon = document.getElementById('icon');
+  //clear video
+  player.innerHTML = " ";
+  //icon
+  if (player.play){
+    getIcon.classList.remove('fa-play');
+    getIcon.classList.add('fa-pause');
+  }
   //vide la file d'attente
   document.getElementById('queue').innerHTML = "";
+  //clear title
+  document.getElementById('title').innerHTML = "";
   //affichage message d'attente
   document.getElementById('wait').innerHTML = "Please wait...";
   //requete_ajax
   var requete_ajax = new XMLHttpRequest();
-  var url = "https://cors-anywhere.herokuapp.com/http://rss.cnn.com/services/podcasting/studentnews/rss.xml";
+  var clean_url = document.querySelector('#rsslink').value;
+  var url = "https://cors-anywhere.herokuapp.com/"+clean_url;
   requete_ajax.open('GET', url, true);
   requete_ajax.ContentType = "text/xml";
   requete_ajax.overrideMimeType('text/xml');
@@ -60,7 +127,7 @@ function addRss(){
       var source = req.getElementsByTagName("link")[0];
       document.getElementById("p_titre").innerHTML = titre.childNodes[0].nodeValue;
       document.getElementById("p_source").innerHTML = source.childNodes[0].nodeValue;
-      var items = [];
+      var titles = [];
       fetch(url).then((res) => {
         res.text().then((xmlTxt) => {
         var domParser = new DOMParser()
@@ -76,13 +143,19 @@ function addRss(){
           //suppression message d'attente
           document.getElementById('wait').innerHTML = "";
           //ajout des titres dans une liste
-          document.getElementById('queue').innerHTML += '<li>' + current_title + '</li>';
-          items.push(item);
+          titles.unshift(current_title);
+          //affichage titre
+          document.getElementById('title').innerHTML = current_title;
+          player.load();
+        });
+        titles.forEach(function(element) {
+          document.getElementById('queue').innerHTML += '<li>' + element + '</li>';
         });
       });
     });
   }
 };
+//progress bar
 var player = document.querySelector('#videoPlayer');
 var progress = document.querySelector('#progress');
 var progressBar = document.querySelector('#progress-bar');
@@ -101,5 +174,4 @@ player.addEventListener('timeupdate', function() {
    progress.value = player.currentTime;
    progressBar.style.width = Math.floor((player.currentTime / player.duration) * 100) + '%';
 });
-
 }
